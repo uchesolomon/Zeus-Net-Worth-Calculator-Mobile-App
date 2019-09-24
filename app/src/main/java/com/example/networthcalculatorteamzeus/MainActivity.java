14db4f76c110470f41a0a537a7217eb03cdf48bc
uchesolomon <solomon7373@yahoo.com>
2019-09-24 23:38:23 +0100

    Second Commit

---
+++ app/src/main/java/com/example/networthcalculatorteamzeus/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.networthcalculatorteamzeus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText emailLogin = (EditText) findViewById(R.id.emailLogin);
+        final EditText passwordLogin = (EditText) findViewById(R.id.passwordLogin);
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        Button registerButton = (Button) findViewById(R.id.registerButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailLogin.getText().toString();
+                String password = passwordLogin.getText().toString();
+                SharedPreferences preferences = getSharedPreferences("MYPREFS", MODE_PRIVATE);
+
+                String userDetails = preferences.getString(email + password + "data", "You are not a registered user yet. Please sign up");
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("display",userDetails);
+                editor.commit();
+
+                Intent dashboard = new Intent(MainActivity.this, DashboardActivity.class);
+                startActivity(dashboard);
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerScreen = new Intent(MainActivity.this, SignUp.class);
+                startActivity(registerScreen);
+            }
+        });
+    }
+}
