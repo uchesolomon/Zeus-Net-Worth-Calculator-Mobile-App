c3bde414bbfabd48a566f883d5cb1ceaf6a7fee2
uchesolomon <solomon7373@yahoo.com>
2019-09-25 02:50:25 +0100

    Third Commit

--- app/src/main/java/com/example/networthcalculatorteamzeus/DashboardActivity.java
+++ app/src/main/java/com/example/networthcalculatorteamzeus/DashboardActivity.java
@@ -2,9 +2,14 @@ package com.example.networthcalculatorteamzeus;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Locale;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -18,5 +23,24 @@ public class DashboardActivity extends AppCompatActivity {
 
         TextView displayInfo = (TextView) findViewById(R.id.dashText);
         displayInfo.setText(display);
+
+        final EditText assets = (EditText) findViewById(R.id.assetValue);
+        final EditText cash = (EditText) findViewById(R.id.cashValue);
+        final EditText liabilities = (EditText) findViewById(R.id.liabilityValue);
+        final TextView netWorth = (TextView) findViewById(R.id.netWorthValue);
+        Button btnCalculate = (Button) findViewById(R.id.btnNetWorth);
+
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double assetVal = Double.parseDouble(assets.getText().toString());
+                double cashVal = Double.parseDouble(cash.getText().toString());
+                double liabilityVal = Double.parseDouble(liabilities.getText().toString());
+                double result = assetVal + cashVal - liabilityVal;
+                netWorth.setText(String.format(Locale.getDefault(), "%f", result));
+
+            }
+        });
     }
 }
